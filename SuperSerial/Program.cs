94e53f3a9e3cc1014667a8521f295bf066bd4f91
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-02-14 08:39:17 -0500

    Final SuperSerial

--- SuperSerial/Program.cs
+++ SuperSerial/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Microsoft.Win32;
+using MsrLib;
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Diagnostics;
 
 namespace SuperSerial
 {
@@ -12,11 +13,74 @@ namespace SuperSerial
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] parameters)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int cancel = 10;
+            foreach(string param in parameters)
+            {
+                cancel = int.Parse(param);
+                break;
+            }
+            Console.WriteLine("SuperSerial: " + Assembly.GetEntryAssembly().GetName().Version);
+            Console.WriteLine("Initializing...");
+            MsrLibKernel.AutoAttachNewDevices = true;
+            MsrLibKernel.Initialize();
+            MsrDeviceLane lane = MsrLibKernel.GetLane(0, true);
+            lane.AutoAttachNewDevices = true;
+            lane.DeviceAttached += Lane_DeviceAttached;
+            Console.WriteLine("Looking for a device... ");
+            MsrLibKernel.FindDeviceAsync(true);
+            while (cancel > 0)
+            {
+                Console.WriteLine(cancel--);
+                Thread.Sleep(1000);
+            }
+            try
+            {
+                RegistryKey regKey32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).CreateSubKey(@"SOFTWARE\SuperSerial", true);
+                regKey32.SetValue("AppProduct", "");
+                regKey32.SetValue("AppSerialNumber", "");
+                regKey32.SetValue("AppSerialNumber2", "");
+                regKey32.Close();
+            }
+            catch (Exception) { }
+            try
+            {
+                RegistryKey regKey64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).CreateSubKey(@"SOFTWARE\SuperSerial", true);
+                regKey64.SetValue("AppProduct", "");
+                regKey64.SetValue("AppSerialNumber", "");
+                regKey64.SetValue("AppSerialNumber2", "");
+                regKey64.Close();
+            }
+            catch (Exception) { }
+        }
+
+        private static void Lane_DeviceAttached(object sender, EventArgs e)
+        {
+            Console.WriteLine("Found a device! Reading Data...");
+            MsrDeviceLane lane = (MsrDeviceLane)sender;
+            Console.WriteLine("AppProduct: " + lane.Device.Properties.AppProduct);
+            Console.WriteLine("AppSerialNumber: " + lane.Device.Properties.AppSerialNumber);
+            Console.WriteLine("AppSerialNumber2: " + lane.Device.Properties.AppSerialNumber2);
+            try
+            {
+                RegistryKey regKey32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).CreateSubKey(@"SOFTWARE\SuperSerial", true);
+                regKey32.SetValue("AppProduct", lane.Device.Properties.AppProduct);
+                regKey32.SetValue("AppSerialNumber", lane.Device.Properties.AppSerialNumber);
+                regKey32.SetValue("AppSerialNumber2", lane.Device.Properties.AppSerialNumber2);
+                regKey32.Close();
+            }
+            catch (Exception) { }
+            try
+            {
+                RegistryKey regKey64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).CreateSubKey(@"SOFTWARE\SuperSerial", true);
+                regKey64.SetValue("AppProduct", lane.Device.Properties.AppProduct);
+                regKey64.SetValue("AppSerialNumber", lane.Device.Properties.AppSerialNumber);
+                regKey64.SetValue("AppSerialNumber2", lane.Device.Properties.AppSerialNumber2);
+                regKey64.Close();
+            }
+            catch (Exception) { }
+            Process.GetCurrentProcess().Kill();
         }
     }
 }
